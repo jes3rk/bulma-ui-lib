dc4defee616f1b875bb0716a93ef1edbbf8ced83
jes3rk <joseph.schefer@gmail.com>
2020-12-18 23:26:01 +0100

    Fixed ts

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,12 @@ import dts from 'rollup-plugin-dts';
 export default [{
         input: 'dist/index.js',
         output: {
-            file: './lib/index.js'
+            file: 'lib/index.js'
         },
     }, {
         input: 'dist/index.d.ts',
         output: {
-            file: './lib/index.d.ts'
+            file: 'lib/index.d.ts'
         },
         plugins: [dts()]
     }
