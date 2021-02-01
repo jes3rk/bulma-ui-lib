54989cddf99852f29483725381d7cf5831810909
jes3rk <joseph.schefer@gmail.com>
2021-02-01 13:04:55 -0500

    fixed reference

--- src/layout/Columns.tsx
+++ src/layout/Columns.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BaseHTMLProps, stringPropDefaulter } from "../BaseElements";
+import { BaseHTMLProps, stringPropCombineDefaulter } from "../BaseElements";
 
 export interface ColProps extends BaseHTMLProps {
   children?: JSX.Element | JSX.Element[];
@@ -7,7 +7,7 @@ export interface ColProps extends BaseHTMLProps {
 
 export const Column = (props: ColProps): JSX.Element => {
   const _props: ColumnContainerProps = { ...props };
-  _props.className = stringPropDefaulter(_props.className, "column");
+  _props.className = stringPropCombineDefaulter(_props.className, "column");
   return <div>{props.children}</div>;
 };
 
@@ -17,6 +17,6 @@ export interface ColumnContainerProps extends BaseHTMLProps {
 
 export const ColumnContainer = (props: ColumnContainerProps): JSX.Element => {
   const _props: ColumnContainerProps = { ...props };
-  _props.className = stringPropDefaulter(_props.className, "columns");
+  _props.className = stringPropCombineDefaulter(_props.className, "columns");
   return <div {..._props}>{props.children}</div>;
 };
